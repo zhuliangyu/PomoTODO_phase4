4c099cc8e27112509bca5cfe295529d89c279e34
Ricky Zhu <ricky@enbut.com>
2019-03-15 23:05:31 -0700

    V3

--- src/main/controller/TodobarController.java
+++ src/main/controller/TodobarController.java
@@ -12,9 +12,6 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import model.Task;
-import ui.EditTask;
-import ui.ListView;
-import ui.PomoTodoApp;
 import utility.Logger;
 
 import java.io.File;
@@ -26,20 +23,34 @@ import java.util.ResourceBundle;
 public class TodobarController implements Initializable {
     private static final String todoOptionsPopUpFXML = "resources/fxml/TodoOptionsPopUp.fxml";
     private static final String todoActionsPopUpFXML = "resources/fxml/TodoActionsPopUp.fxml";
-    
+    // TODO: 2019-03-15
+    private File todoOptionsPopupFile = new File(todoOptionsPopUpFXML);
+    private File todoActionsPopupFile = new File(todoActionsPopUpFXML);
+
+    // TODO: 2019-03-15
+    private JFXPopup todobarPopUp;
+    private JFXPopup viewPopUp;
+
+
     @FXML
     private Label descriptionLabel;
+
     @FXML
     private JFXHamburger todoActionsPopUpBurger;
     @FXML
     private StackPane todoActionsPopUpContainer;
+
     @FXML
     private JFXRippler todoOptionsPopUpRippler;
     @FXML
     private StackPane todoOptionsPopUpBurger;
-    
+
+
+
+
+
     private Task task;
-    
+
     // REQUIRES: task != null
     // MODIFIES: this
     // EFFECTS: sets the task in this Todobar
@@ -48,9 +59,119 @@ public class TodobarController implements Initializable {
         this.task = task;
         descriptionLabel.setText(task.getDescription());
     }
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO: complete this method
+        loadTodobarPopUp();
+        loadTodobarPopUpActionListener();
+        loadTodoOptionsPopUp();
+        loadTodoOptionsPopUpActionListener();
     }
+
+    private void loadTodoOptionsPopUpActionListener() {
+        todoOptionsPopUpBurger.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                viewPopUp.show(todoOptionsPopUpBurger,
+                        JFXPopup.PopupVPosition.TOP,
+                        JFXPopup.PopupHPosition.RIGHT,
+                        12,
+                        15);
+            }
+        });
+    }
+
+
+    private void loadTodoOptionsPopUp() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(todoOptionsPopupFile.toURI().toURL());
+            fxmlLoader.setController(new ViewOptionsPopUpController());
+            viewPopUp = new JFXPopup(fxmlLoader.load());
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void loadTodobarPopUpActionListener() {
+        todoActionsPopUpContainer.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                todobarPopUp.show(todoActionsPopUpContainer,
+                        JFXPopup.PopupVPosition.TOP,
+                        JFXPopup.PopupHPosition.LEFT,
+                        -12,
+                        15);
+            }
+        });
+    }
+
+
+    // TODO: 2019-03-15
+    private void loadTodobarPopUp() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(todoActionsPopupFile.toURI().toURL());
+            fxmlLoader.setController(new TodobarPopUpController());
+            todobarPopUp = new JFXPopup(fxmlLoader.load());
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+    }
+
+    //todo
+    class TodobarPopUpController {
+        //todo
+        @FXML
+        private JFXListView<?> actionPopUpList;
+
+        @FXML
+        private void submit() {
+            int selectedIndex = actionPopUpList.getSelectionModel().getSelectedIndex();
+            switch (selectedIndex) {
+                case 0:
+                    Logger.log("TodobarOptionsPopUpController", "TODO");
+                    break;
+                case 1:
+                    Logger.log("TodobarOptionsPopUpController", "upnext");
+                    break;
+                case 2:
+                    Logger.log("TodobarOptionsPopUpController", "in progress");
+                    break;
+                case 3:
+                    Logger.log("TodobarOptionsPopUpController", "done");
+                    break;
+                case 4:
+                    Logger.log("TodobarOptionsPopUpController", "pomodoro");
+                    break;
+                default:
+                    Logger.log("TodobarOptionsPopUpController", "No action is implemented for the selected option");
+            }
+            todobarPopUp.hide();
+        }
+    }
+
+    // Inner class: view selector pop up controller
+    class ViewOptionsPopUpController {
+        @FXML
+        private JFXListView<?> optionPopUpList;
+
+        @FXML
+        private void submit() {
+            int selectedIndex = optionPopUpList.getSelectionModel().getSelectedIndex();
+            switch (selectedIndex) {
+                case 0:
+                    Logger.log("TodobarViewOptionsPopUpController", "edit");
+                    break;
+                case 1:
+                    Logger.log("TotobarViewOptionsPopUpController", "delete");
+                    break;
+
+                default:
+                    Logger.log("ToolbarViewOptionsPopUpController", "No action is implemented for the selected option");
+            }
+            viewPopUp.hide();
+        }
+    }
+
 }
